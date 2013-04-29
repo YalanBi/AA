4352fc541401619d7baa561de54b44da238d01c7
YalanBi <biyalan@yeah.net>
2013-04-29 18:44:18 +0200

    change with chr length, new ways to show QTLs higher than thre and update for new FMD mapping

--- functions/cis-transPlot_expGene_FM.r
+++ functions/cis-transPlot_expGene_FM.r
@@ -1,27 +1,206 @@
 #
 # Functions for analysing A. Thaliana Tiling Arrays
-# last modified: 15-04-2013
+# last modified: 29-04-2013
 # first written: 15-04-2013
 # (c) 2013 GBIC Yalan Bi, Danny Arends, R.C. Jansen
 #
 
+
+#******************************************************** chrLength = max probe bp ********************************************************#
+#*************************************************************** basic part ***************************************************************#
+setwd("D:/Arabidopsis Arrays")
+
+probebp <- vector("list", 5)
+totlength <- 0
+for(chr in 1:5){
+  st <- proc.time()
+  
+  cat("chr", chr, "position loading...\n")
+  #probebp[[chr]] <- read.table(paste("Data/fullModeMapping/expGenes_chr", chr, "_1tu1probe.txt", sep=""), row.names=1, header=TRUE)[,"bp"]
+  #another way to load exon location (bp)
+  fbp <- file(paste("Data/fullModeMapping/expGenes_chr", chr, "_1tu1probe.txt", sep=""))
+  open(fbp)
+  invisible(readLines(fbp, 1))
+  pline <- readLines(fbp, 1)
+  while(length(pline) > 0 && pline != ""){
+    probebp[[chr]] <- c(probebp[[chr]], as.numeric(strsplit(pline,"\t")[[1]][2]))
+    pline <- readLines(fbp, 1)
+  }
+  close(fbp)
+  
+  totlength <- c(totlength, max(probebp[[chr]]))
+  et <- proc.time()
+  cat("chr", chr, "got bp after", (et-st)[3], "secs!\n")
+}
+#totlength <- c(0, 30414756, 19685885, 23459479, 18581266, 26968948)
+
+
+#load map file---markers' Morgan position
+mpos <- read.table("refined map/map_addMAXprobebp_nogap.txt", row.names=1, header=T)
+
+
+#for x, use marker bp; for y, use probe bp
+lpos <- NULL
+for(chr in 1:5){
+  #add gap between chrs, then get markers' bp position!
+  mpos[which(mpos[,1] == chr),2] <- (mpos[which(mpos[,1] == chr),2] + 6000000*(chr-1))/1000000
+  lpos <- c(lpos, (sum(totlength[1:chr]) + 6000000*(chr-1))/1000000, (sum(totlength[1:(chr + 1)]) + 6000000*(chr-1))/1000000)
+}
+#lpos(0.00000, 30.41476, 36.41476, 56.10064, 62.10064, 85.56012, 91.56012, 110.14139, 116.14139, 143.11033)
+
+
+#*********************************************************** plot QTL cis-trans ***********************************************************#
+lodthreshold = 8.0
+
+png(filename = paste0("cis-trans_QTL_", lodthreshold, ".png"), width = 1024, height = 1024, bg = "white")
+plot(x = c(4, max(lpos)-4), y = c(4, max(lpos)-4), t='n', xlab="marker position (Mb)", ylab="Gene position (Mb)", xaxt='n', yaxt='n')
+axis(1, at=seq(0, 150, 10), labels=seq(0, 150, 10))
+axis(2, at=seq(0, 150, 10), labels=seq(0, 150, 10))
+
+for(r in 1:4){
+  rect(lpos[r*2], -90, lpos[r*2+1], max(lpos)+90, col=rgb(159, 182, 205, 105, max = 255), border="transparent")
+  rect(-90, lpos[r*2], max(lpos)+90, lpos[r*2+1], col=rgb(159, 182, 205, 105, max = 255), border="transparent")
+}
+#abline(h=lpos[seq(1,10,2)], v=lpos[seq(1,10,2)],col='green',lty=2)
+#abline(h=lpos[seq(2,10,2)], v=lpos[seq(2,10,2)],col='red',lty=2)
+
+for(chr in 1:5){
+  st <- proc.time()
+  
+  cat("chr", chr, "QTL loading and plotting ...\n")
+  fp <- file(paste("Data/fullModeMapping/expGenes_chr", chr, "_FMD_QTL.txt", sep=""))
+  open(fp)
+  mline <- readLines(fp, 1)
+  
+  for(p in 1:length(probebp[[chr]])){
+    mline <- readLines(fp, 1)
+    qtl <- as.numeric(strsplit(mline,"\t")[[1]][2:717])
+    col2 <- qtl
+    col2[qtl < lodthreshold] <- rgb(1, 1, 1,0)
+    col2[qtl >= lodthreshold] <- rgb(238, 59, 59, alpha=120, maxColorValue=255) #>= +thre is red
+    col2[qtl <= -lodthreshold] <- rgb(102, 205, 0, alpha=120, maxColorValue=255) #<= -thre is green
+    points(x = mpos[,2], y = (rep(probebp[[chr]][p], 716) + sum(totlength[1:chr]) + 6000000*(chr-1))/1000000, col = col2, pch = 20, cex = 0.5)
+  }
+  
+  close(fp)
+  
+  et <- proc.time()
+  cat("chr", chr, "finished plotting after", (et-st)[3], "secs!\n")
+}
+dev.off()
+
+
+#*********************************************************** plot Int cis-trans ***********************************************************#
+intthreshold = 11.6
+
+png(filename = paste0("cis-trans_Int_", intthreshold, ".png"), width = 1024, height = 1024, bg = "white")
+plot(x = c(4, max(lpos)-4), y = c(4, max(lpos)-4), t='n', xlab="marker position (Mb)", ylab="Gene position (Mb)", xaxt='n', yaxt='n')
+axis(1, at=seq(0, 150, 10), labels=seq(0, 150, 10))
+axis(2, at=seq(0, 150, 10), labels=seq(0, 150, 10))
+
+for(r in 1:4){
+  rect(lpos[r*2], -90, lpos[r*2+1], max(lpos)+90, col=rgb(159, 182, 205, 105, max = 255), border="transparent")
+  rect(-90, lpos[r*2], max(lpos)+90, lpos[r*2+1], col=rgb(159, 182, 205, 105, max = 255), border="transparent")
+}
+#abline(h=lpos[seq(1,10,2)], v=lpos[seq(1,10,2)],col='green',lty=2)
+#abline(h=lpos[seq(2,10,2)], v=lpos[seq(2,10,2)],col='red',lty=2)
+
+for(chr in 1:5){
+  st <- proc.time()
+  
+  cat("chr", chr, "Int loading and plotting ...\n")
+  fp <- file(paste("Data/fullModeMapping/expGenes_chr", chr, "_FMD_Int.txt", sep=""))
+  open(fp)
+  mline <- readLines(fp, 1)
+  
+  for(p in 1:length(probebp[[chr]])){
+    mline <- readLines(fp, 1)
+    int <- as.numeric(strsplit(mline,"\t")[[1]][2:717])
+    col2 <- int
+    col2[int < intthreshold] <- rgb(1, 1, 1,0)
+    col2[int >= intthreshold] <- rgb(238, 59, 59, alpha=120, maxColorValue=255)#"gray80"
+    points(x = mpos[,2], y = (rep(probebp[[chr]][p], 716) + sum(totlength[1:chr]) + 6000000*(chr-1))/1000000, col = col2, pch = 20, cex = 0.5)
+  }
+  
+  close(fp)
+  
+  et <- proc.time()
+  cat("chr", chr, "finished plotting after", (et-st)[3], "secs!\n")
+}
+dev.off()
+
+
+
+########################################################### NEVER USE AGAIN !!!! ###########################################################
+#********************************************************** remove trans markers **********************************************************#
+#find trans markers with plots
+mbp <- read.table("refined map/map.physical.c15o15f.txt", row.names=1, header=T)
+mcm <- read.table("refined map/map.txt", row.names=1, header=T)
+totlength <- c(0, 30414756, 19685885, 23459479, 18581266, 26968948)
+
+addlength_cM <- 0
+for(chr in 1:5){
+  mbp[which(mbp[,1]==chr),2] <- mbp[which(mbp[,1]==chr),2] + sum(totlength[1:chr])
+  
+  addlength_cM <- c(addlength_cM, addlength_cM[chr] + max(mcm[which(mcm[,1]==chr),2]))
+  mcm[which(mcm[,1]==chr),2] <- mcm[which(mcm[,1]==chr),2] + addlength_cM[chr] + 25*(chr-1)
+}
+#addlength_cM(0, 123.2792, 196.7278, 289.9451, 391.9624, 505.9731)
+
+
+#show all chrs within one plot
+png(filename = "chrall_bp-cM.png", width = 1024, height = 1024, bg = "white")
+plot(mcm[,2], mbp[,2], col=mbp[,1], pch=20, main="Before Remove Markers", xlab="cM", ylab="bp")
+dev.off()
+
+
+#replace trans markers' bp position with NA, and make a new map file
+mbp[which(mbp[,1] != mcm[,1]),2] <- NA
+mbp["27610",2] <- NA
+mbp["126099",2] <- NA
+mbp["329114",2] <- NA
+write.table(mbp, file="refined map/map_addMAXprobebp_nogap.txt", sep="\t")
+mbp_new <- read.table("refined map/map_addMAXprobebp_nogap.txt", row.names=1, header=T)
+
+#show all chrs within one plot
+png(filename = "chrall_bp-cM_remove.png", width = 1024, height = 1024, bg = "white")
+plot(mcm[,2], mbp_new[,2], col=mbp_new[,1], pch=20, main="After Remove Markers", xlab="cM", ylab="bp")
+dev.off()
+########################################################### NEVER USE AGAIN !!!! ###########################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#********************************************************* chrLength = TOT length *********************************************************#
 #*************************************************************** basic part ***************************************************************#
 setwd("D:/Arabidopsis Arrays")
 
 #load map file---markers' Morgan position
-mpos <- read.table("refined map/map_bp_remove_nogap.txt", row.names=1, header=T)
-
+mpos <- read.table("refined map/map_addTOTlength_nogap.txt", row.names=1, header=T)
 
 #for x, use marker bp; for y, use probe bp
 totlength <- c(0, 34964571, 22037565, 25499034, 20862711, 31270811)
-chrEnds <- c(161, 262, 402, 526, 716)
 lpos <- NULL
 for(chr in 1:5){
   #add gap between chrs, then get markers' bp position!
-  mpos[which(mpos[,1] == chr),2] <- (mpos[which(mpos[,1] == chr),2] + 6000000*(chr-1))/1000
-  lpos <- c(lpos, (sum(totlength[1:chr]) + 6000000*(chr-1))/1000, (sum(totlength[1:(chr + 1)]) + 6000000*(chr-1))/1000)
+  mpos[which(mpos[,1] == chr),2] <- (mpos[which(mpos[,1] == chr),2] + 6000000*(chr-1))/1000000
+  lpos <- c(lpos, (sum(totlength[1:chr]) + 6000000*(chr-1))/1000000, (sum(totlength[1:(chr + 1)]) + 6000000*(chr-1))/1000000)
 }
-#lpos(0.00, 34964.57, 40964.57, 63002.14, 69002.14, 94501.17, 100501.17, 121363.88, 127363.88, 158634.69)
+#lpos(0.00000, 34.96457, 40.96457, 63.00214, 69.00214, 94.50117, 100.50117, 121.36388, 127.36388, 158.63469)
 
 
 #************************************************************** plot cis-trans **************************************************************#
@@ -29,13 +208,13 @@ lodthreshold = 8
 lodthreshold_black = 12
 
 png(filename = paste0("cis-trans_exon_", lodthreshold, "+", lodthreshold_black, ".png"), width = 1024, height = 1024, bg = "white")
-plot(x = c(5000, max(mpos, na.rm=T)-1000), y = c(5000, max(lpos)-5000), t='n', xlab="QTL position (kb)", ylab="Gene position (kb)", xaxt='n', yaxt='n')
-axis(1, at=seq(0, 150000, 10000), labels=seq(0, 150000, 10000))
-axis(2, at=seq(0, 150000, 10000), labels=seq(0, 150000, 10000))
+plot(x = c(5, max(lpos)-5), y = c(5, max(lpos)-5), t='n', xlab="QTL position (Mb)", ylab="Gene position (Mb)", xaxt='n', yaxt='n')
+axis(1, at=seq(0, 160, 10), labels=seq(0, 160, 10))
+axis(2, at=seq(0, 160, 10), labels=seq(0, 160, 10))
 
 for(r in 1:4){
-  rect(lpos[r*2], -9000000, lpos[r*2+1], max(lpos)+9000000, col=rgb(159, 182, 205, 105, max = 255), border="transparent")
-  rect(-9000000, lpos[r*2], max(lpos)+9000000, lpos[r*2+1], col=rgb(159, 182, 205, 105, max = 255), border="transparent")
+  rect(lpos[r*2], -90, lpos[r*2+1], max(lpos)+90, col=rgb(159, 182, 205, 105, max = 255), border="transparent")
+  rect(-90, lpos[r*2], max(lpos)+90, lpos[r*2+1], col=rgb(159, 182, 205, 105, max = 255), border="transparent")
 }
 #abline(h=lpos[seq(1,10,2)], v=lpos[seq(1,10,2)],col='green',lty=2)
 #abline(h=lpos[seq(2,10,2)], v=lpos[seq(2,10,2)],col='red',lty=2)
@@ -52,7 +231,7 @@ for(chr in 1:5){
   pline <- readLines(fbp, 1)
   expbp <- NULL
   while(length(pline) > 0 && pline != ""){
-    expbp <- c(expbp, as.numeric(strsplit(pline,"\t")[[1]][2])/1000)
+    expbp <- c(expbp, as.numeric(strsplit(pline,"\t")[[1]][2]))
     pline <- readLines(fbp, 1)
   }
   close(fbp)
@@ -71,7 +250,7 @@ for(chr in 1:5){
     col2[qtl < lodthreshold] <- rgb(1, 1, 1,0)
     col2[qtl >= lodthreshold & qtl < lodthreshold_black] <- "gray80"
     col2[qtl >= lodthreshold_black] <- "black"
-    points(x = mpos[,2], y = rep(expbp[p], 716) + (sum(totlength[1:chr]) + 6000000*(chr-1))/1000, col = col2, pch = 20, cex = 0.5)
+    points(x = mpos[,2], y = (rep(expbp[p], 716) + sum(totlength[1:chr]) + 6000000*(chr-1))/1000000, col = col2, pch = 20, cex = 0.5)
   }
   
   close(fp)
@@ -80,8 +259,6 @@ for(chr in 1:5){
   cat("chr", chr, "finished plotting after", (et-st)[3], "secs!\n")
 }
 dev.off()
-
-
 
 
 
@@ -114,8 +291,8 @@ mbp[which(mbp[,1] != mcm[,1]),2] <- NA
 mbp["27610",2] <- NA
 mbp["126099",2] <- NA
 mbp["329114",2] <- NA
-write.table(mbp, file="refined map/map_bp_remove_nogap.txt", sep="\t")
-mbp_new <- read.table("refined map/map_bp_remove_nogap.txt", row.names=1, header=T)
+write.table(mbp, file="refined map/map_addTOTlength_nogap.txt", sep="\t")
+mbp_new <- read.table("refined map/map_addTOTlength_nogap.txt", row.names=1, header=T)
 
 #show all chrs within one plot
 png(filename = "chrall_bp-cM_remove.png", width = 1024, height = 1024, bg = "white")
