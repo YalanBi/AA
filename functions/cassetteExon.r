b5fb13b6bbca4bf725e5f0f72b578a047cc1fb1f
YalanBi <biyalan@yeah.net>
2013-05-02 14:57:23 +0200

    not finished...

---
+++ functions/cassetteExon.r
@@ -0,0 +1,26 @@
+#
+# Functions for analysing A. Thaliana Tiling Arrays
+# last modified: 02-05-2013
+# first written: 02-05-2013
+# (c) 2013 GBIC Yalan Bi, Danny Arends, R.C. Jansen
+#
+
+#*************************************************************** basic part ***************************************************************#
+setwd("D:/Arabidopsis Arrays")
+
+
+#direction selection
+probesDir <- function(exp_data = rawexp){
+  if(unique(exp_data[,"strand"]) == "sense"){
+    direction_id <- which(exp_data[, "direction"] == "reverse")
+  }
+  if(unique(exp_data[,"strand"]) == "complement"){
+    direction_id <- which(exp_data[, "direction"] == "forward")
+  }
+  return(direction_id)
+}
+
+
+function(expdata, exon, use=(mean, median)){
+  
+}
