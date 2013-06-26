492f4a9977edff535740d8a22418e3513341520c
YalanBi <biyalan@yeah.net>
2013-06-26 17:18:19 +0200

    remove SNP probes, before mapping

--- functions/removeSNP_beforeMapping.r
+++ functions/removeSNP_beforeMapping.r
@@ -5,10 +5,10 @@
 # (c) 2013 GBIC Yalan Bi, Danny Arends, R.C. Jansen
 #
 
-#*************************************************************** basic part ***************************************************************#
+#**************************************************** this is the final version for removing SNP probes in the genes ^_^ ***************************************************#
 setwd("D:/Arabidopsis Arrays")
 #load SNP probes' number, snp is a list of 475009 integers
-snp <- read.table("snp ids.txt")[,1]
+snp <- read.table("snp ids.txt")[ ,1]
 
 
 removeSNP <- function(filename = "AT1G01010", location, verbose = FALSE){
@@ -18,11 +18,11 @@ removeSNP <- function(filename = "AT1G01010", location, verbose = FALSE){
     if(verbose) cat(filename, "has SNP in it!")
 
     #save old exp file with name "AT1G01010_SNPin.txt"
-    write.table(rawexp, file = paste0(location, filename, "_SNPin.txt"))
+    write.table(rawexp, file=paste0(location, filename, "_SNPin.txt"))
 
     #save new exp file with name "AT1G01010.txt"
     rawexp <- rawexp[-which(rownames(rawexp) %in% snp), ]
-    write.table(rawexp, file = paste0(location, filename, ".txt"))
+    if(nrow(rawexp) > 0) write.table(rawexp, file=paste0(location, filename, ".txt"))
 
     if(verbose) cat(" and old exp+new exp files saved!\n")
   } else if(verbose) cat(filename, "no SNP\n")
@@ -44,4 +44,3 @@ for(chr in 1:5){
   et <- proc.time()[3]
   cat("and chr", chr, "finished in", et-st, "s!\n")
 }
-
